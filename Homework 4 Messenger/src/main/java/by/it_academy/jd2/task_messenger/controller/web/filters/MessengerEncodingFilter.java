099ba47db9780c0a2d0142ead64327975b3274fa
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-08-20 22:40:27 +0300

    fix encodingFilter

--- Homework 4 Messenger/src/main/java/by/it_academy/jd2/task_messenger/controller/web/filters/MessengerEncodingFilter.java
+++ Homework 4 Messenger/src/main/java/by/it_academy/jd2/task_messenger/controller/web/filters/MessengerEncodingFilter.java
@@ -4,17 +4,23 @@ import javax.servlet.*;
 import java.io.IOException;
 
 public class MessengerEncodingFilter implements Filter {
-    private String encoding = null;
+    private String encoding;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         encoding = filterConfig.getInitParameter("encoding");
+        if (encoding==null){
+            encoding="UTF-8";
+        }
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        request.setCharacterEncoding(encoding);
-        response.setContentType("text/html; charset=" + encoding);
+        if (null==request.getCharacterEncoding()){
+            request.setCharacterEncoding(encoding);
+        }
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 
