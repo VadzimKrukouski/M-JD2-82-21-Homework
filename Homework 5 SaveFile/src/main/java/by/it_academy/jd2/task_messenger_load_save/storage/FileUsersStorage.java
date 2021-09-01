b8ac2b95bf3483c89a1dd4a3810d6e611acc0264
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-09-01 09:50:58 +0300

    fix SaveLoadFile bugs

--- Homework 5 SaveFile/src/main/java/by/it_academy/jd2/task_messenger_load_save/storage/FileUsersStorage.java
+++ Homework 5 SaveFile/src/main/java/by/it_academy/jd2/task_messenger_load_save/storage/FileUsersStorage.java
@@ -4,8 +4,13 @@ import by.it_academy.jd2.task_messenger_load_save.model.User;
 import by.it_academy.jd2.task_messenger_load_save.storage.api.IUsersStorage;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileUsersStorage implements IUsersStorage {
     private static final FileUsersStorage instance = new FileUsersStorage();
@@ -24,7 +29,7 @@ public class FileUsersStorage implements IUsersStorage {
         String login = user.getLogin();
         try (FileWriter writer = new FileWriter(file)) {
             String newLine = System.getProperty("line.separator");
-          if (getUser(login)==null){
+            if (getUser(login) == null) {
                 writer.append(user.getLogin());
                 writer.append(",");
                 writer.append(user.getPassword());
@@ -46,22 +51,31 @@ public class FileUsersStorage implements IUsersStorage {
     @Override
     public User getUser(String login) {
         User user = new User();
+        if (Files.exists(Path.of(path))) {
+//            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+//                String line = reader.readLine();
+//                Stream<String> lines = line.lines();
+//                List<String> strings = lines.collect(Collectors.toList());
+            try {
+                Stream<String> lines = Files.lines(Path.of(path));
+                List<String> strings = lines.collect(Collectors.toList());
+                for (String string : strings) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line = reader.readLine();
-            while (line != null) {
-                String[] partsString = line.split(",");
-                if (login.equals(partsString[0])) {
-                    user.setLogin(partsString[0]);
-                    user.setPassword(partsString[1]);
-                    user.setFio(partsString[2]);
-                    user.setBirthday(partsString[3]);
-                    user.setRegistration(partsString[4]);
-                    return user;
+//            while (line != null) {
+                    String[] partsString = string.split(",");
+                    if (login.equals(partsString[0])) {
+                        user.setLogin(partsString[0]);
+                        user.setPassword(partsString[1]);
+                        user.setFio(partsString[2]);
+                        user.setBirthday(partsString[3]);
+                        user.setRegistration(partsString[4]);
+                        return user;
+                    }
                 }
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
             }
-        } catch (IOException exception) {
-            exception.printStackTrace();
         }
         return null;
     }
@@ -69,21 +83,30 @@ public class FileUsersStorage implements IUsersStorage {
     @Override
     public Collection<User> getAll() {
         Collection<User> users = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line = reader.readLine();
-            while (line != null) {
-                String[] partsString = line.split(",");
-                User user = new User();
+//        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+//            String line = reader.readLine();
+//            Stream<String> lines = line.lines();
+//            List<String> strings = lines.collect(Collectors.toList());
+        if (Files.exists(Path.of(path))) {
+
+            try {
+                Stream<String> lines = Files.lines(Path.of(path));
+                List<String> strings = lines.collect(Collectors.toList());
+                for (String string : strings) {
+//                while (line != null) {
+                    String[] partsString = string.split(",");
+                    User user = new User();
                     user.setLogin(partsString[0]);
                     user.setFio(partsString[2]);
                     user.setBirthday(partsString[3]);
                     user.setRegistration(partsString[4]);
                     users.add(user);
                 }
+            } catch (IOException exception) {
+                exception.printStackTrace();
             }
-         catch (IOException exception) {
-            exception.printStackTrace();
         }
+
         return users;
     }
 }
