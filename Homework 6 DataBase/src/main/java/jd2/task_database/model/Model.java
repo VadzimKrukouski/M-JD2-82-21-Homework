46ec58992fbf413d28f3079300fc75e4182eb703
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-09-04 19:26:58 +0300

    init

---
+++ Homework 6 DataBase/src/main/java/jd2/task_database/model/Model.java
@@ -0,0 +1,22 @@
+package jd2.task_database.model;
+
+public class Model {
+    private String name;
+    private String  salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
