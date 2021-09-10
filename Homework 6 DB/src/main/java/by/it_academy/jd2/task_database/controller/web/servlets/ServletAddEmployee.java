3b2068dd4ed7ca86bc14e67742ce0565a7eaca12
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-09-10 23:42:47 +0300

    fix3

--- Homework 6 DB/src/main/java/by/it_academy/jd2/task_database/controller/web/servlets/ServletAddEmployee.java
+++ Homework 6 DB/src/main/java/by/it_academy/jd2/task_database/controller/web/servlets/ServletAddEmployee.java
@@ -9,7 +9,6 @@ import by.it_academy.jd2.task_database.view.PositionService;
 import by.it_academy.jd2.task_database.view.api.IDepartmentService;
 import by.it_academy.jd2.task_database.view.api.IEmployeeService;
 import by.it_academy.jd2.task_database.view.api.IPositionService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,12 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 
-@WebServlet (name = "ServletAddEmployee", urlPatterns = "/addEmployee")
+@WebServlet(name = "ServletAddEmployee", urlPatterns = "/addEmployee")
 public class ServletAddEmployee extends HttpServlet {
     private final IEmployeeService employeeService;
     private final IDepartmentService departmentService;
     private final IPositionService positionService;
-    private final ObjectMapper mapper = new ObjectMapper();
 
     public ServletAddEmployee() {
         this.employeeService = EmployeeService.getInstance();
@@ -39,32 +37,32 @@ public class ServletAddEmployee extends HttpServlet {
 
         req.setAttribute("allDepartments", allDepartments);
         req.setAttribute("allPositions", allPositions);
-        req.getRequestDispatcher("views/addEmployee.jsp").forward(req,resp);
+        req.getRequestDispatcher("views/addEmployee.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String name = req.getParameter("name");
-//        double salary = Double.parseDouble((req.getParameter("salary")));
-//        long departmentId = Long.parseLong(req.getParameter("department"));
-//        long positionId = Long.parseLong(req.getParameter("position"));
+        String name = req.getParameter("name");
+        double salary = Double.parseDouble((req.getParameter("salary")));
+        long departmentId = Long.parseLong(req.getParameter("department"));
+        long positionId = Long.parseLong(req.getParameter("position"));
 
-//        Department department = departmentService.getDepartment(departmentId);
-//        Position position = positionService.getPosition(positionId);
+        Department department = departmentService.getDepartment(departmentId);
+        Position position = positionService.getPosition(positionId);
 
-        Employee employee = mapper.readValue(req.getInputStream(), Employee.class);
-//        employee.setName(name);
-//        employee.setSalary(salary);
-//        employee.setDepartment(department);
-//        employee.setPosition(position);
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setSalary(salary);
+        employee.setDepartment(department);
+        employee.setPosition(position);
 
         long id = employeeService.addEmployee(employee);
-        if (id>0){
+        if (id > 0) {
             req.setAttribute("id", id);
             req.setAttribute("info", "Сотрудник успешно добавлен с id=");
         } else {
             req.setAttribute("infoErr", "Сотрудник не добавлен");
         }
-        req.getRequestDispatcher("views/addEmployee.jsp").forward(req,resp);
+        req.getRequestDispatcher("views/addEmployee.jsp").forward(req, resp);
     }
 }
