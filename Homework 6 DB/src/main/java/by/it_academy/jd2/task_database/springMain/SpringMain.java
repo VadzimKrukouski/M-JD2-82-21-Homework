07495b5f1044bbcb8a9eaef79f4ca00495e2bb03
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-10-07 16:05:32 +0300

    add SpringMain

--- Homework 6 DB/src/main/java/by/it_academy/jd2/task_database/springMain/SpringMain.java
+++ Homework 6 DB/src/main/java/by/it_academy/jd2/task_database/springMain/SpringMain.java
@@ -1,5 +1,7 @@
 package by.it_academy.jd2.task_database.springMain;
 
+import by.it_academy.jd2.task_database.model.Employee;
+import by.it_academy.jd2.task_database.view.EmployeeServiceHibernate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +9,9 @@ public class SpringMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("service.xml", "storage.xml");
 
+        EmployeeServiceHibernate bean = context.getBean(EmployeeServiceHibernate.class);
+        Employee employee = bean.getEmployee(1);
+        System.out.println(employee.toString());
 
 
     }
