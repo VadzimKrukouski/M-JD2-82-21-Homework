90563c5f5f2a42c8d23918ceeaf24f8b538662fc
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-10-29 15:02:42 +0300

    fix

--- Final Project/src/main/java/com/example/demo/controller/ProductController.java
+++ Final Project/src/main/java/com/example/demo/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
