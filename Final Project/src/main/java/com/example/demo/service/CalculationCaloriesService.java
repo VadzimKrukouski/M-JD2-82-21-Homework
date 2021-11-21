11d840e39ab47e373f078924c2ea406f8865f454
vkrukovski <vadim.krukovski@pstlabs.by>
2021-11-21 18:15:23 +0300

    update calculationCalories

--- Final Project/src/main/java/com/example/demo/service/CalculationCaloriesService.java
+++ Final Project/src/main/java/com/example/demo/service/CalculationCaloriesService.java
@@ -16,43 +16,43 @@ public class CalculationCaloriesService implements ICalculationCaloriesService {
     @Override
     public CalculationCaloriesDTO calculation(List<JournalFood> journalFoodList) {
         CalculationCaloriesDTO caloriesDTO = new CalculationCaloriesDTO();
+        double calories = 0;
+        double proteins = 0;
+        double fats = 0;
+        double carbohydrates = 0;
+        double weight = 0;
         for (JournalFood journalFood : journalFoodList) {
-            double currentFoodWeight = journalFood.getWeight();
             if (journalFood.getProduct() != null) {
                 Product product = journalFood.getProduct();
                 double defaultWeightProduct = product.getWeight();
+                double currentProductWeight = journalFood.getWeight();
                 caloriesDTO.setName(product.getName());
-                caloriesDTO.setCalories(currentFoodWeight * product.getCalories() / defaultWeightProduct);
-                caloriesDTO.setProteins(currentFoodWeight * product.getProteins() / defaultWeightProduct);
-                caloriesDTO.setFats(currentFoodWeight * product.getFats() / defaultWeightProduct);
-                caloriesDTO.setCarbohydrates(currentFoodWeight * product.getCarbohydrates() / defaultWeightProduct);
-                caloriesDTO.setWeight(currentFoodWeight);
+                calories += currentProductWeight * product.getCalories() / defaultWeightProduct;
+                proteins += currentProductWeight * product.getProteins() / defaultWeightProduct;
+                fats += currentProductWeight * product.getFats() / defaultWeightProduct;
+                carbohydrates += currentProductWeight * product.getCarbohydrates() / defaultWeightProduct;
+                weight += currentProductWeight;
             }
             if (journalFood.getRecipe() != null) {
-                double calories = 0;
-                double proteins = 0;
-                double fats = 0;
-                double carbohydrates = 0;
-                double weight = 0;
                 Recipe recipe = journalFood.getRecipe();
+                caloriesDTO.setName(recipe.getName());
                 List<ComponentDish> componentDishes = recipe.getComponentDishes();
                 for (ComponentDish componentDish : componentDishes) {
                     Product product = componentDish.getProduct();
                     double defaultWeightProduct = product.getWeight();
                     double currentProductWeight = componentDish.getWeightProduct();
-                    calories+=currentProductWeight*product.getCalories()/defaultWeightProduct;
-                    proteins+=currentProductWeight*product.getProteins()/defaultWeightProduct;
-                    fats+=currentProductWeight*product.getFats()/defaultWeightProduct;
-                    carbohydrates+=currentProductWeight*product.getCarbohydrates()/defaultWeightProduct;
-                    weight+=componentDish.getWeightProduct();
+                    calories += currentProductWeight * product.getCalories() / defaultWeightProduct;
+                    proteins += currentProductWeight * product.getProteins() / defaultWeightProduct;
+                    fats += currentProductWeight * product.getFats() / defaultWeightProduct;
+                    carbohydrates += currentProductWeight * product.getCarbohydrates() / defaultWeightProduct;
+                    weight += currentProductWeight;
                 }
-                caloriesDTO.setName(recipe.getName());
-                caloriesDTO.setCalories(calories);
-                caloriesDTO.setProteins(proteins);
-                caloriesDTO.setFats(fats);
-                caloriesDTO.setCarbohydrates(carbohydrates);
-                caloriesDTO.setWeight(weight);
             }
+            caloriesDTO.setCalories(calories);
+            caloriesDTO.setProteins(proteins);
+            caloriesDTO.setFats(fats);
+            caloriesDTO.setCarbohydrates(carbohydrates);
+            caloriesDTO.setWeight(weight);
         }
         return caloriesDTO;
     }
